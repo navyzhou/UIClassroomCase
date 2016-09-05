65c6c0e8949c65ea936f1a2531f35b0cc003ac39
jp413670706 <jp_706@126.com>
2016-09-05 21:30:01 +0800

    mysql数据的操作

---
+++ mysql/connectionagain.js
@@ -0,0 +1,41 @@
+/**
+ * Created by Administrator on 16-9-5.
+ */
+var mysql=require("mysql");
+var connection=mysql.createConnection({
+    host:"127.0.0.1",
+    port:3306,
+    database:"stusys",
+    user:"root",
+    password:"a"
+});
+
+function getConnect(){
+    connection=mysql.createConnection({
+        host:"127.0.0.1",
+        port:3306,
+        database:"stusys",
+        user:"root",
+        password:"a"
+    });
+
+    connection.connect(function(err){
+        console.info(err);
+       if(err){
+           console.info("数据库连接失败..");
+       } else {
+           console.info("数据库连接成功...");
+       }
+    });
+}
+
+connection.on("error",function(err){
+   if(err.code==="PROTOCOL_CONNECTION_LOST"){
+       console.info("与mysql数据库的连接被断开...");
+   } else{
+       console.info(err);
+       throw err; //抛出错误
+   }
+});
+
+getConnect();
--- mysql/mysqlupdate.js
+++
@@ -1,5 +0,0 @@
-/**
- * Created by Administrator on 16-9-5.
- */
-
-
