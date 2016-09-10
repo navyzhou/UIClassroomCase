72f26fa1c662490bac48cefb95e1092025cfee27
jp413670706 <jp_706@126.com>
2016-09-10 22:02:02 +0800

    学生信息管理系统

--- stusys/server.js
+++ stusys/server.js
@@ -25,18 +25,20 @@ server.on("request",function(req,res){
    if(req.url!="/favicon.ico"){
         var urlObj=url.parse(req.url); //将url变成一个json对象
         var path=urlObj.pathname;
-        console.info(path);
+
         if(path=="/"){
             readFile("./login.html",res);
         }else if(path=="/getAllClassInfo"){ //获取所有班级信息
             pool.getConnection(function(err,connection){
                if(err){
-                   res.write("{code:0}");
+                   res.writeHeader(200,"OK",{"Content-Type":"text/json"});
+                   res.write('{"code":"0"}');
                    res.end();
                }else{
                     connection.query("select * from classInfo order by cid asc",function(err,rows){
                        if(err){
-                           res.write("{code:1}");
+                           res.writeHeader(200,"OK",{"Content-Type":"text/json"});
+                           res.write('{"code":"1"}');
                            res.end();
                        }else{
                            res.write(JSON.stringify(rows));
@@ -44,6 +46,32 @@ server.on("request",function(req,res){
                        }
                     });
                }
+            });
+        }else if(path=="/adduser"){ //说明是学生注册
+            //注意此时我们使用的是POST提交方式，数据不在url里面，所以不能直接从url中获取。
+            //POST提交数据时，请求头信息和数据是分开传送的，先发送请求头信息，然后在发送数据，所以我们需要通过监听来完成数据接收
+            //获取学生的注册信息
+            req.on("data",function(data){
+                var dataInfo=querystring.parse(data.toString());
+                dataInfo["sid"]=0;
+
+                //将用户提交的注册信息存入数据库
+                pool.getConnection(function(err,connection){
+                    if(err){ //如果获取连接失败，则返回一个0
+                        res.write("0");
+                        res.end();
+                    }else{
+                        console.info(dataInfo);
+                        connection.query("insert into stuInfo set ?",dataInfo,function(err,result){
+                            if(err){
+                                res.write("1"); //数据添加失败
+                            }else{
+                                res.write(result.insertId+"");
+                            }
+                            res.end();
+                        });
+                    }
+                });
             });
         }else{
             readFile("."+path,res);
@@ -55,14 +83,23 @@ server.on("request",function(req,res){
 
 //读取指定路径的文件
 function readFile(path,res){
-    var file=fs.createReadStream(path);
-    file.on("data",function(data){
-       res.write(data);
-    });
+    //先判断文件是否存在
+    fs.exists(path,function(exsits){
+        if(exsits){
+            var file = fs.createReadStream(path);
+            file.on("data", function (data) {
+                res.write(data);
+            });
 
-    file.on("end",function(){
-        res.end();
-    });
+            file.on("end", function () {
+                res.end();
+            });
+        }else{
+            res.writeHeader(404,"Not Found",{"Content-Type":"text/html;charset=utf-8"});
+            res.write("<h1>404页面未找到...</h1>");
+            res.end();
+        }
+    })
     //如果是fileRead读文件的话，必须判断要读取的文件类型
     //fs.readFile(path,"binary",function(err,data){});
     //fs.readFile(path,"utf8",function(err,data){});
