f9d4c0481274cfb784882f905fd712ffbff20e58
jp413670706 <jp_706@126.com>
2016-09-22 17:28:16 +0800

    商品信息管理

--- goods/page/js/index.js
+++ goods/page/js/index.js
@@ -21,10 +21,32 @@ function showRegister(){
     $("#zcpages").mywin({left:"center",top:"center"});
     $("#loginpages").hide();
     $(".bg").fadeIn("200","linear");
+    $("#registertishi").html("");
 }
 
 //关闭注册窗口
 function hidenzcpage(){
     $("#zcpages").hide();
     $(".bg").fadeOut();
+}
+
+//用户注册
+function userzc(){
+    var uname= $.trim($("#zcuname").val());
+    var pwd=$.trim($("#zcpwd").val());
+    var pwdagain=$.trim($("#zcpwdagain").val());
+
+    $.post("userRegister",{uname:uname,pwd:pwd,pwdagain:pwdagain},function(data){
+        data=$.trim(data);
+        switch(data){
+            case "1":$("#registertishi").text("用户名不能为空...");break;
+            case "2":$("#registertishi").text("密码不能为空...");break;
+            case "3":$("#registertishi").text("两次密码输入不一致...");break;
+            case "4":$("#registertishi").text("数据库连接失败...");break;
+            case "5":$("#registertishi").text("数据添加失败...");break;
+            case "6":$("#registertishi").text("注册成功..");hidenzcpage();
+                break;
+            default:$("#registertishi").text("注册失败..");break;
+        }
+    },"text");
 }
--- goods/server.js
+++ goods/server.js
@@ -11,6 +11,55 @@ var app=express(); //创建一个应用程序
 //使用静态中间件
 app.use(express.static("page")); //默认到page文件夹下查找静态资源
 
+//配置和使用body-parser中间件
+app.use(bodyparser.urlencoded({extended:false}));
+
+//配置和使用session中间件
+app.use(session({
+    secret: 'keyboard cat',
+    resave: true,
+    saveUninitialized: true,
+    cookie: {secure:false,maxAge:1000*60*2}
+}));
+
+//配置文件上传的中间件
+var upload =multer({dest:"./page/pic"}); //上传图片的目录设定
+
+//配置数据库连接池
+var pool =mysql.createPool({
+    host:"127.0.0.1",
+    port:3306,
+    database:"goods",
+    user:"root",
+    password:"a"
+});
+
+//处理用户注册的方法
+app.post("/userRegister",function(req,res){
+    var result="0";
+    if(req.body.uname==""){
+        res.send("1"); //说明用户名为空
+    }else if(req.body.pwd==""){
+        res.send("2"); //说明密码为空
+    }else if(req.body.pwd!=req.body.pwdagain){
+        res.send("3"); //说明两次密码不一致
+    }else{//访问数据库
+        pool.getConnection(function(err,connection){
+           if(err){
+               res.send("4"); //说明数据库连接失败
+           } else {
+               connection.query("insert into adminInfo values(0,?,?)",[req.body.uname,req.body.pwd],function(err,result){
+                   if(err){
+                       res.send("5"); //说明添加数据失败
+                   }else{
+                       res.send("6"); //注册成功
+                   }
+               })
+           }
+        });
+    }
+});
+
 app.listen(80,function(err){
     if(err){
         console.info(err);
